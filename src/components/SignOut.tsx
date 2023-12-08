dea13da9715c034909ce011413ebf6ecd5cdead8
AminRsh <amin.sharifi45@gmail.com>
2023-12-07 23:51:59 -0500

    full

---
+++ src/components/SignOut.tsx
@@ -0,0 +1,12 @@
+import { signOut } from 'firebase/auth';
+import { auth } from '../utils/firebase.config';
+
+const SignOut: React.FC = () => {
+    return (
+        <div>
+            <button onClick={() => signOut(auth)}>Sign Out</button>
+        </div>
+    );
+};
+
+export default SignOut;
