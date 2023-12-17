7412796f642575c7364595a1745802ad3f29dce3
AminRsh <amin.sharifi45@gmail.com>
2023-12-17 02:33:53 -0500

    Nothing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,7 +10,7 @@ export default defineConfig({
       '/api': {
         target: 'https://api.pexels.com/v1',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
