c5761ab84e890ca0e081ae826158aa884a8c3a22
AminRsh <amin.sharifi45@gmail.com>
2023-12-17 16:44:57 -0500

    Search.js

---
+++ src/api/search.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export default async function handler(req, res) {
+    try {
+        const response = await axios.get(`https://api.pexels.com/v1/search?query=${req.query.query}&per_page=1`, {
+            headers: {
+                Authorization: import.meta.env.VITE_FIREBASE_PEXELS_API_KEY
+            }
+        });
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch images' });
+    }
+}
