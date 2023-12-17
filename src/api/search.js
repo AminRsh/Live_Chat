a946746544d419e09b5cf88944726aad074a3203
AminRsh <amin.sharifi45@gmail.com>
2023-12-17 17:04:40 -0500

    catch log

--- src/api/search.js
+++ src/api/search.js
@@ -11,6 +11,7 @@ export default async function handler(req, res) {
         });
         res.status(200).json(response.data);
     } catch (error) {
+        console.error('Error:', error);
         res.status(500).json({ error: 'Failed to fetch images' });
     }
 }
