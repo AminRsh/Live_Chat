1a7339dcba7cc171119fcc9840368e3be4c7eddc
AminRsh <amin.sharifi45@gmail.com>
2023-12-17 17:14:08 -0500

    Error from Search:

--- src/api/search.js
+++ src/api/search.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
         });
         res.status(200).json(response.data);
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Error from Search:', error);
         res.status(500).json({ error: 'Failed to fetch images' });
     }
 }
