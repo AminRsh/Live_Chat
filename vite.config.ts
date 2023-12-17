bf3fd3f4245119ac3151c9e5cf10381702809eb3
AminRsh <amin.sharifi45@gmail.com>
2023-12-17 17:35:55 -0500

    commenting vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8080,
-    proxy: {
-      '/api': {
-        target: 'https://api.pexels.com/v1',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  // server: {
+  //   port: 8080,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://api.pexels.com/v1',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // }
 })
