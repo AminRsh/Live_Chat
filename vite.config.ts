05a1f196583f26620e696d29383482a8b9b17efd
AminRsh <amin.sharifi45@gmail.com>
2023-12-17 02:28:00 -0500

    Api

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,10 @@ export default defineConfig({
       '/api': {
         target: 'https://api.pexels.com/v1',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        headers: {
+          Authorization: "kAjCHUL3B0us6dbJBna3ANSI4z2SeQknxKSYbRufppFsKysW5uOkAtxC", 
+        },
       }
     }
   }
