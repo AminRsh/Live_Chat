eaa2227b9844bc5dec8170c5b3c7b886a439cf39
AminRsh <amin.sharifi45@gmail.com>
2023-12-18 11:53:48 -0500

    Final_Se

--- api/search.js
+++ api/search.js
@@ -4,8 +4,7 @@ export default async function handler(req, res) {
     try {
         const response = await axios.get(`https://api.pexels.com/v1/search?query=${req.query.query}&per_page=1`, {
             headers: {
-                // Authorization: `Bearer ${process.env.VITE_FIREBASE_PEXELS_API_KEY}`
-                Authorization: "kAjCHUL3B0us6dbJBna3ANSI4z2SeQknxKSYbRufppFsKysW5uOkAtxC"
+                Authorization: `Bearer ${process.env.VITE_FIREBASE_PEXELS_API_KEY}`
             }
         });
         res.status(200).json(response.data);
